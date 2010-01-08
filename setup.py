122c1a8afae035c4f5508ae763e6cabcc2986f84
Jari Pennanen <jari.pennanen@gmail.com>
2010-01-09 00:03:09 +0200

    Project initial (2).

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import sys
+assert sys.version >= '2.5', "Requires Python v2.5 or above"
+from distutils.core import setup, Extension
+
+setup(
+    name="songdetails",
+    version="0.1",
+    author="Jari Pennanen",
+    author_email="jari.pennanen@gmail.com",
+    url="http://github.com/Ciantic/songdetails/",
+    description="Song details retrieval package.",
+    long_description="Retrieves Song detail information such as artist, title, album duration, etc.",
+    license="FreeBSD",
+    packages=["songdetails"],
+    package_dir={'songdetails': 'src/songdetails'}
+)
